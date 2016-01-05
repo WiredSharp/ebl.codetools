de1b472fc2a0b6f71918079a5bb248683c16314d
eric brunel <ebrunel@ofi-am.fr>
2016-01-05 15:06:14 +0100

    fix builddir handling (inverted test)

--- sources/VSProjectNormalizer/VSProjectNormalizer.cs
+++ sources/VSProjectNormalizer/VSProjectNormalizer.cs
@@ -16,11 +16,9 @@ namespace VSProjectNormalizer
 
 		public class Settings
 		{
-			private const string DEFAULT_OUTPUT_PREFIX_WITH_PLATFORM = @"$(SolutionDir)$(Configuration).$(Platform)\";
-			private const string BUILD_DIR_WITH_PLATFORM = @"$(BuildDir)$(SolutionName)\$(Configuration).$(Platform)\";
-
-			private const string DEFAULT_OUTPUT_PREFIX = @"$(SolutionDir)$(Configuration)\";
-			private const string BUILD_DIR = @"$(BuildDir)$(SolutionName)\$(Configuration)\";
+			private const string WITH_PLATFORM = @".$(Platform)\";
+			private const string DEFAULT_BUILD_FOLDER = @"$(SolutionDir)$(Configuration)";
+			private const string SPECIFIC_BUILD_FOLDER = @"$(BuildDir)$(SolutionName)\$(Configuration)";
 
 			private const string ACCEPTANCE_TEST_OUTPUT_PATH = @"acceptance\$(AssemblyName)";
 			private const string TEST_OUTPUT_PATH = @"tests\$(AssemblyName)";
@@ -28,6 +26,10 @@ namespace VSProjectNormalizer
 			private const string INTERMEDIATE_OUTPUT_PATH = @"obj\$(AssemblyName)";
 
 			private static readonly Lazy<Settings> DEFAULT = new Lazy<Settings>();
+			private string _acceptanceTestOutputPath;
+			private string _testOutputPath;
+			private string _binOutputPath;
+			private string _intermediateOutputPath;
 
 			public static Settings Default
 			{
@@ -38,21 +40,49 @@ namespace VSProjectNormalizer
 
 			public string BuildPrefix
 			{
-				get { return UsePlatform ? BUILD_DIR_WITH_PLATFORM : BUILD_DIR; }
+				get { return UsePlatform ? SPECIFIC_BUILD_FOLDER + WITH_PLATFORM : SPECIFIC_BUILD_FOLDER + @"\"; }
 			}
 
 			public string DefaultBuildPrefix
 			{
-				get { return UsePlatform ? DEFAULT_OUTPUT_PREFIX_WITH_PLATFORM : DEFAULT_OUTPUT_PREFIX; }
+				get { return UsePlatform ? DEFAULT_BUILD_FOLDER + WITH_PLATFORM : DEFAULT_BUILD_FOLDER + @"\"; }
 			}
 
-			public string AcceptanceTestOutputPath { get; set; }
+			public string AcceptanceTestOutputPath
+			{
+				get
+				{
+					return String.IsNullOrEmpty(_acceptanceTestOutputPath) ? ACCEPTANCE_TEST_OUTPUT_PATH : _acceptanceTestOutputPath;
+				}
+				set { _acceptanceTestOutputPath = value; }
+			}
 
-			public string TestOutputPath { get; set; }
+			public string TestOutputPath
+			{
+				get
+				{
+					return String.IsNullOrEmpty(_testOutputPath) ? TEST_OUTPUT_PATH : _testOutputPath;
+				}
+				set { _testOutputPath = value; }
+			}
 
-			public string BinOutputPath { get; set; }
+			public string BinOutputPath
+			{
+				get
+				{
+					return String.IsNullOrEmpty(_binOutputPath) ? BIN_OUTPUT_PATH : _binOutputPath;
+				}
+				set { _binOutputPath = value; }
+			}
 
-			public string IntermediateOutputPath { get; set; }
+			public string IntermediateOutputPath
+			{
+				get
+				{
+					return String.IsNullOrEmpty(_intermediateOutputPath) ? INTERMEDIATE_OUTPUT_PATH : _intermediateOutputPath;
+				}
+				set { _intermediateOutputPath = value; }
+			}
 
 			public string BuildPath { get; set; }
 
@@ -98,8 +128,8 @@ namespace VSProjectNormalizer
 			XNamespace defaultNamespace = root.GetDefaultNamespace();
 			RemoveNodes(root, OUTPUT_PATH_TAG, BUILD_DIRECTORY_TAG, INTERMEDIATE_OUTPUT_TAG);
 			XElement commonPropertyGroup = GetFirstCommonPropertyGroup(root);
-			var buildTagExists = new XAttribute("Condition", " '$(" + BUILD_DIRECTORY_TAG + ")' == '' ");
-			var buildTagNotExists = new XAttribute("Condition", " '$(" + BUILD_DIRECTORY_TAG + ")' != '' ");
+			var buildTagExists = new XAttribute("Condition", " '$(" + BUILD_DIRECTORY_TAG + ")' != '' ");
+			var buildTagNotExists = new XAttribute("Condition", " '$(" + BUILD_DIRECTORY_TAG + ")' == '' ");
 			if (!String.IsNullOrEmpty(CurrentSettings.BuildPath))
 			{
 				commonPropertyGroup.Add(new XElement(defaultNamespace.GetName(BUILD_DIRECTORY_TAG),
