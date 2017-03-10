479b19dc5643712fe171420cf065afe11acf3e7c
eric brunel <ebrunel@ofi-am.fr>
2017-03-10 17:27:27 +0100

    update publish script

--- publish.bat
+++ publish.bat
@@ -1,1 +1,3 @@
-xcopy /e /d Release\bin\VSProjectNormalizer C:\bin\VSProjectNormalizer
+xcopy /e /d /f Artifacts\Release\bin\VSProjectNormalizer\*.exe C:\bin\VSProjectNormalizer
+xcopy /e /d /f Artifacts\Release\bin\VSProjectNormalizer\*.dll C:\bin\VSProjectNormalizer
+xcopy /e /d /f Artifacts\Release\bin\VSProjectNormalizer\*.config C:\bin\VSProjectNormalizer
